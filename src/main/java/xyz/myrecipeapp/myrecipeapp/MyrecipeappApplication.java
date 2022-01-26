f66c4c1412aeb0ebd07b7a339f7074f910bde158
KamilChabros <kamilchabros@wp.pl>
2022-01-26 20:06:08 +0100

    Empty project files added

---
+++ src/main/java/xyz/myrecipeapp/myrecipeapp/MyrecipeappApplication.java
@@ -0,0 +1,13 @@
+package xyz.myrecipeapp.myrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrecipeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyrecipeappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/myrecipeapp/myrecipeapp/MyrecipeappApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.myrecipeapp.myrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrecipeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
