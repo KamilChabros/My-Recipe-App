a2cd329c21ce72c6ff253b6fbffdd5b6393bc32b
KamilChabros <kamilchabros@wp.pl>
2022-02-05 17:34:26 +0100

    MyUserDetailsService class added with implementation of UserDetails; UserDetailsServiceImpl class added with implementation of UseDetailsService (from Spring); WebSecurityConfig config class added;

---
+++ src/main/java/xyz/myrecipeapp/myrecipeapp/services/MyUserDetailsService.java
@@ -0,0 +1,62 @@
+package xyz.myrecipeapp.myrecipeapp.services;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import xyz.myrecipeapp.myrecipeapp.model.Role;
+import xyz.myrecipeapp.myrecipeapp.model.User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+public class MyUserDetailsService implements UserDetails {
+
+    private User user;
+
+    public MyUserDetailsService(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<Role> roles = user.getRoles();
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        for (Role role: roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
---
+++ src/main/java/xyz/myrecipeapp/myrecipeapp/services/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package xyz.myrecipeapp.myrecipeapp.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import xyz.myrecipeapp.myrecipeapp.model.User;
+import xyz.myrecipeapp.myrecipeapp.repositories.UserRepository;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.getUserByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Could not find user!");
+        }
+        return new MyUserDetailsService(user);
+    }
+}
