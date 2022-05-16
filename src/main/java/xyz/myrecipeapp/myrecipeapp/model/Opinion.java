ba23de2f20dd04d03fe0db598aaa39740c682090
KamilChabros <kamilchabros@wp.pl>
2022-05-16 20:55:36 +0200

    Opinion.java class with some code added;

---
+++ src/main/java/xyz/myrecipeapp/myrecipeapp/model/Opinion.java
@@ -0,0 +1,58 @@
+package xyz.myrecipeapp.myrecipeapp.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "opinion")
+public class Opinion implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String rating;
+    private String content;
+    private String date;
+
+    public Opinion() {
+    }
+
+    public Opinion(Long id, String rating, String content, String date) {
+        this.id = id;
+        this.rating = rating;
+        this.content = content;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
